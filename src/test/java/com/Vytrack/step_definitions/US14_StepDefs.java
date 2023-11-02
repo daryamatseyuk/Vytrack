235ed67cc326cbe8403f94615f705aec32206fa8
daryamatseyuk <matdasha95@gmail.com>
2023-11-01 21:40:11 -0400

    US14 finished AC01

--- src/test/java/com/Vytrack/step_definitions/US14_StepDefs.java
+++ src/test/java/com/Vytrack/step_definitions/US14_StepDefs.java
@@ -1,36 +1,55 @@
 package com.Vytrack.step_definitions;
 
+import com.Vytrack.pages.HomePage;
+import com.Vytrack.pages.LoginPage;
+import com.Vytrack.pages.MarketingCampaignsPage;
+import com.Vytrack.utilities.BrowserUtils;
+import com.Vytrack.utilities.ConfigurationReader;
+import com.Vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class US14_StepDefs {
 
+    LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
+    MarketingCampaignsPage marketingCampaignsPage = new MarketingCampaignsPage();
+
     @Given("user is on the login page. Dasha")
     public void user_is_on_the_login_page_dasha() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
     }
-    @When("user selects {string} option in {string} module. Dasha")
-    public void user_selects_option_in_module_dasha(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("user logs in as a {string}. Dasha")
+    public void user_logs_in_as_a_dasha(String role) {
+        loginPage.loginAs(role);
+    }
+    @Then("user selects {string} option in {string} module. Dasha")
+    public void user_selects_option_in_module_dasha(String module, String option) {
+        homePage.selectOptionFromModules(module, option);
     }
     @Then("user clicks on the filter button. Dasha")
     public void user_clicks_on_the_filter_button_dasha() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        marketingCampaignsPage.filterBtn.click();
     }
     @Then("clicks on the manage filters button. Dasha")
     public void clicks_on_the_manage_filters_button_dasha() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        marketingCampaignsPage.manageFiltersDropdown.click();
     }
-    @Then("user is able to see options below. Dasha")
-    public void user_is_able_to_see_options_below_dasha() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @Then("user is able to see options below checked by default. Dasha")
+    public void user_is_able_to_see_options_below_checked_by_default_dasha(List<String>expectedOptionsDropdown) {
+        for (WebElement eachWebElement : marketingCampaignsPage.optionFromDropdown) {
+            Assert.assertTrue(eachWebElement.isSelected());
+        }
     }
+
+
 
 
 }
