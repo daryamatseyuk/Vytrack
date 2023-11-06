775571afe99dbbe4c4e225539167db19e620f071
Ouballa7 <ouballa7@gmail.com>
2023-11-06 17:51:47 -0500

    My user story is ready

---
+++ src/test/java/com/Vytrack/pages/AllCarsPage.java
@@ -0,0 +1,33 @@
+package com.Vytrack.pages;
+
+import com.Vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AllCarsPage {
+
+    public AllCarsPage() { PageFactory.initElements(Driver.getDriver(),this);}
+
+    @FindBy(xpath = "//table//a[@class='dropdown-toggle']")
+    public WebElement threeDots;
+
+    @FindBy(xpath = "//ul[contains(@class,'launchers-list')]//li[@class='launcher-item']//a")
+    public List<WebElement> iconsList;
+
+    public void moveToThreeDots() {
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(threeDots).perform();
+    }
+
+    public List<String> getIcons() {
+        moveToThreeDots();
+        return iconsList.stream().map(x -> x.getAttribute("title")).collect(Collectors.toList());
+    }
+
+
+}
+
---
+++ src/test/resources/features/US06_EditCar.feature
@@ -0,0 +1,21 @@
+@B30G20-175
+Feature: Default
+
+
+	@B30G20-174
+	Scenario Outline: Edit car info for all users
+		Given user logs in as "<role>"
+				When user selects "Vehicles" option from "Fleet" module
+				And user hovers over 3 dots"..."
+				Then user is able to see following icons
+					| View    |
+					| Edit |
+					| Delete  |
+		
+				
+		
+				Examples:
+					| role          |
+					| store manager |
+					| sales manager |
+		            | driver        |
